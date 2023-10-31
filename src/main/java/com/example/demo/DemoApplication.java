e9829649397a83a06490321d3da444aac84997cd
franxxdmsk132 <tapiafranklin666@gmail.com>
2023-10-30 21:53:08 -0500

    ,

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+@GetMapping("/mensaje")
+
+    public String mensaje () {
+        return "welcome";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
